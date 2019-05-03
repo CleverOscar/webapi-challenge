4b43a6555fb52e90af8f3e88c3052de5532d7cd9
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-05-03 11:54:34 -0500

    Modules connected

---
+++ data/actions-router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const Helpers = require('./helpers/actionModel');
+const router = express.Router();
+
+console.log(Helpers);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const server = express();
+const projectRouter = require('./data/projects-router');
+const actionsRouter = require('./data/actions-router');
+
+server.use(express.json());
 
 server.get('/', (req, res) =>{
   res.send(
